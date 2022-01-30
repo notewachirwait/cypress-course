becf380bf1ae8936a10e95e268486303d88cedb9
Note <wachirawit.th@ku.th>
2022-01-30 14:02:21 +0700

    commit

--- cypress/integration/example/intercept.test.js
+++ cypress/integration/example/intercept.test.js
@@ -53,20 +53,3 @@ describe("intercept example", () => {
 // {
 // 	log: false,
 //   }
-
-// before(() => {
-//   // runs once before all tests
-// });
-
-// beforeEach(() => {
-//   // root-level hook
-//   // runs before every test block
-// });
-
-// afterEach(() => {
-//   // runs after each test block
-// });
-
-// after(() => {
-//   // runs once all tests are done
-// });
